ee8887fd64aaeb4eb37bdf49290fdc8841a40279
Mohammed Yussif <yussif4112@gmail.com>
2026-03-09 00:10:26 -0700

    Fix CSP to allow inline scripts for admin dashboard

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(helmet({
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc:    ["'self'", "https://fonts.gstatic.com"],
-      scriptSrc:  ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
       imgSrc:     ["'self'", "data:"],
       connectSrc: ["'self'"],
     }
